26b555f677621074334e2c353e146bbc441be96f
unknown <j3415879@naver.com>
2024-11-11 17:00:05 +0900

    test controller

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,8 +1,17 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.MemberDAO;
 
 @Service
 public class MemberService {
 
+	@Autowired 
+	MemberDAO memberDAO;
+	
+	public String test() {
+		return "테스트입니다.";
+	}
 }
