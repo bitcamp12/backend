4820f783cd6d2b32a0273c979ee8e4afdc22e8d0
unknown <j3415879@naver.com>
2024-11-18 15:38:33 +0900

    로그인 페이지 완성

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	@GetMapping("login")
+	@GetMapping("admin/login")
 	public String login(Model model) {
         return "login";  // login.html 템플릿을 렌더링
     }
