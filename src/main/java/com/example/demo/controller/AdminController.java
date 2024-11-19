7af2af4a8f5a7c34aefbc669dd1cfa8c9fc5ca86
HyelenaZ <yellomouse05@gmail.com>
2024-11-19 11:26:44 +0900

    feat: create sidebar & admin page

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // Admin = 사이트 관리자
 @Controller
-@RequestMapping(value="admin")
+@RequestMapping(value="/secure/admin")
 public class AdminController {
     
-    @GetMapping(value = {"/", ""})
+    @GetMapping("/index")
 	public String index(Model model) {
+    	//관리자 정보 추후 추가하기 model 어트리뷰트..//
+    	
         return "admin/index";  // index.html 템플릿을 렌더링
     }
 }
