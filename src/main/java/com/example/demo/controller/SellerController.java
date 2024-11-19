286bf7f606cec192454fb8566f6cbad3978f577d
HyelenaZ <yellomouse05@gmail.com>
2024-11-19 11:26:44 +0900

    feat: create sidebar & admin page

--- src/main/java/com/example/demo/controller/SellerController.java
+++ src/main/java/com/example/demo/controller/SellerController.java
@@ -5,11 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// Seller = 공연 관리
 @Controller
-@RequestMapping(value="seller")
+@RequestMapping(value="/secure/seller")
 public class SellerController {
 
-    @GetMapping(value = {"/", ""})
+    @GetMapping("/index")
 	public String index(Model model) {
         return "seller/index";  // index.html 템플릿을 렌더링
     }
