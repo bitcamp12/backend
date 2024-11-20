d0b7686972c1ea91699d8dc82c8355666311975b
luvoice97 <luvoice97@gmail.com>
2024-11-20 19:17:04 +0900

    휴대폰인증

--- src/main/java/com/example/demo/dao/MemberDAO.java
+++ src/main/java/com/example/demo/dao/MemberDAO.java
@@ -1,15 +1,17 @@
 package com.example.demo.dao;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Mapper;
-
 import com.example.demo.dto.member.MemberDTO;
 
 @Mapper
 public interface MemberDAO {
+    
+    @Insert("INSERT INTO member (id, name, password, email, phone, address, gender, sns_token, role) " +
+            "VALUES (#{id}, #{name}, #{password}, #{email}, #{phone}, #{address}, #{gender}, #{snsToken}, #{role})")
+    public int signUp(MemberDTO memberDTO);
 
-	public int signUp(MemberDTO memberDTO);
-
-	public int checkId(String id);
-
-	
+    @Select("SELECT COALESCE(COUNT(*), 0) FROM member WHERE id = #{id}")
+    public int checkId(String id); 
 }
