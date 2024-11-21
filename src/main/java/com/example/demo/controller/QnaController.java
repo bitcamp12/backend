9fb262406c759738a3acf229f2c14d07c4a4eaf2
unknown <tkp1200@naver.com>
2024-11-21 18:33:07 +0900

    관람평/기대평/qa 백엔드 코드

--- src/main/java/com/example/demo/controller/QnaController.java
+++ src/main/java/com/example/demo/controller/QnaController.java
@@ -1,10 +1,22 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.QnaDTO;
+import com.example.demo.entity.Qna;
 import com.example.demo.service.QnaService;
+import com.example.demo.util.ApiResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -13,4 +25,122 @@ public class QnaController {
     
 	@Autowired
 	private QnaService qnaService;
+	
+	@PostMapping("qnaWrite")
+	public ResponseEntity<ApiResponse<Qna>> qnaWrite(@RequestParam("playSeq") int playSeq,
+			@RequestBody QnaDTO qnaDTO) {
+		
+		System.out.println(qnaDTO.getTitle()+qnaDTO.getContent());
+		
+		try {
+			int memberSeq=1;
+			int result=qnaService.qnaWrite(playSeq,memberSeq,qnaDTO.getTitle(),qnaDTO.getContent());
+			if(result==1) {
+				 return  ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200, "성공", null));
+			}
+			else { 
+				 return   ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(404 , "실패", null));
+			 } 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(400, "오류", null));
+		}
+		
+		
+	
+		
+	}
+	
+	
+	//qa list->공연 seq
+	@GetMapping("getQnaList")
+	public ResponseEntity<ApiResponse<List<QnaDTO>>> getQnaList(@RequestParam("playSeq") int playSeq) {
+		
+		List<QnaDTO> list =qnaService.getQnaList(playSeq);
+		try {
+		if(!list.isEmpty()) {
+			
+			return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200, "성공", list));
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(404 , "데이터 없음", list));
+		}
+	} catch (Exception e) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(400, "오류", null));
+	}
+		
+		
+	}
+	
+	
+	
+	//qa one 조회
+	@GetMapping("getQnaOne")
+	public ResponseEntity<ApiResponse<QnaDTO>> getQnaOne(@RequestBody QnaDTO qnaDTO) {
+		QnaDTO DTO=qnaService.getQnaOne(qnaDTO.getQnaSeq());
+		
+		try {
+			if(DTO!=null) {
+				
+				return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200, "성공", DTO));
+			}
+			else {
+				return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(404 , "데이터 없음", DTO));
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(400, "오류", null));
+		}
+	}
+	
+	
+	
+	
+	
+	//aq 수정
+	
+	@PostMapping("updateQna")
+	public ResponseEntity<ApiResponse<Qna>> updateQna(@RequestBody QnaDTO qnaDTO) {
+		
+		try {
+			int result=qnaService.updateQna(qnaDTO.getQnaSeq(),qnaDTO.getTitle(),qnaDTO.getContent());
+			if(result==1) {
+				 return  ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200, "성공", null));
+			}
+			else { 
+				 return   ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(404 , "실패", null));
+			 } 
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(400, "오류", null));
+		}
+		
+		
+	
+		
+	}
+	
+	//qa 삭제
+	@PostMapping("deleteQna")
+	public ResponseEntity<ApiResponse<Qna>> deleteQna(@RequestBody QnaDTO qnaDTO) {
+		int result=qnaService.deleteQna(qnaDTO.getQnaSeq());
+		
+		try {
+			if(result==1) {
+				
+				return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(200, "성공", null));
+			}
+			else {
+				return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(404 , "데이터 없음", null));
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(400, "오류", null));
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/com/example/demo/dao/ReviewAfterDAO.java
+++ src/main/java/com/example/demo/dao/ReviewAfterDAO.java
@@ -17,21 +17,26 @@ public interface ReviewAfterDAO {
 
 	//리뷰작성
 	@Insert("""
-	        INSERT INTO review_after (
-	            play_seq,
-	            member_seq,
-	            content,
-	            rating,
-	            created_date
-	        ) VALUES (
-	            #{playSeq},               
-	            #{memberSeq},             
-	            #{content},               
-	            #{rating},               
-	            SYSDATE                   
-	        )
-	    """)
-	void reviewAWrite(int playSeq, int memberSeq, String content, String rating);
+		    INSERT INTO review_after (
+		        play_seq,
+		        member_seq,
+		        content,
+		        rating,
+		        created_date
+		    ) VALUES (
+		        #{playSeq},               
+		        #{memberSeq},             
+		        #{content},               
+		        #{rating},               
+		        Now()                  
+		    )
+		""")
+		int reviewAWrite(@Param("playSeq") int playSeq,
+		                 @Param("memberSeq") int memberSeq,
+		                 @Param("content") String content,
+		                 @Param("rating") int rating);
+	
+	
 
 	 @Select("""
 		        SELECT * FROM  review_after
@@ -40,28 +45,36 @@ public interface ReviewAfterDAO {
 		    """)
 		    List<ReviewAfterDTO> getReviewAList(@Param("playSeq") int playSeq);
 	 
+	 
+	 
 	 @Select("""
 		        SELECT * FROM  review_after
 		        WHERE review_after_seq = #{reviewAfterSeq}
 		    """)
-	ReviewAfterDTO getReviewA(String reviewAfterSeq);
+	ReviewAfterDTO getReviewA(int  reviewAfterSeq);
 
+	 
+	 
 	 @Update("""
-		        UPDATE review_after
-		        SET 
-		            content = #{content}, 
-		            rating = #{rating},
-		            created_date = CURRENT_TIMESTAMP
-		        WHERE 
-		            review_after_seq = #{reviewAfterSeq}
-		    """)
-	void reviewAUpdate(String reviewAfterSeq, String content, String rating);
+			    UPDATE review_after
+			    SET 
+			        content = #{content}, 
+			        rating = #{rating},
+			        created_date = CURRENT_TIMESTAMP
+			    WHERE 
+			        review_after_seq = #{reviewAfterSeq}
+			""")
+			int reviewAUpdate(@Param("reviewAfterSeq") int reviewAfterSeq,
+			                  @Param("content") String content, 
+			                  @Param("rating") int rating);
+	 
+	 
 
 	 @Delete("""
 		        DELETE FROM review_after
 		        WHERE review_after_seq = #{reviewAfterSeq}
 		    """)
-	void reviewADelete(String reviewAfterSeq);
+	int reviewADelete(int reviewAfterSeq);
 	
 
 	
