2ddd460d84eb0a9b49a108ead23ba699576af5bc
unknown <j3415879@naver.com>
2024-11-22 19:01:31 +0900

    dto 수정

--- src/main/java/com/example/demo/dto/PlayTimeTableDTO.java
+++ src/main/java/com/example/demo/dto/PlayTimeTableDTO.java
@@ -11,13 +11,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PlayTimeTableDTO {
 	private int playTimeTableSeq;
-	private int play_seq;
-	private int theater_seq;
-	private String start_time;
-	private String end_time;
-	private String start_dis_time;
-	private String end_dis_time;
-	private int min_rate;
-	private int max_rate;
-	private LocalDate target_date;
+	private int playSeq;
+	private int theaterSeq;
+	private String startTime;
+	private String endTime;
+	private String startDisTime;
+	private String endDisTime;
+	private int minRate;
+	private int maxRate;
+	private LocalDate targetDate;
 }
