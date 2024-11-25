0f6251877223d25ade0ad55371a326cb1d918b62
luvoice97 <luvoice97@gmail.com>
2024-11-25 16:49:24 +0900

    아이디 찾기

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -263,7 +263,7 @@ public class MemberController {
         	 String certificationCode = "111";
 
             // 전화번호와 인증 코드 저장
-            String phoneNum = smsRequestDto.getPhoneNum();
+        	 String phoneNum = smsRequestDto.getPhoneNum().replaceAll("[^0-9]", "");
             String name =smsRequestDto.getName();
             
             Map<String, String> map = new HashMap<>();
@@ -300,8 +300,37 @@ public class MemberController {
         }
     }
     
+    //회원가입 sms
     @GetMapping("/verifyPhone")
     public ResponseEntity<ApiResponse<String>> verifyPhoneNumberCode(@RequestParam("phoneNum") String phoneNum, @RequestParam("code") String code) {
+        Long expirationTime = codeExpirationTimes.get(phoneNum);
+        System.out.println(phoneNum+code);
+        if (expirationTime != null && System.currentTimeMillis() > expirationTime) {
+            // 만료된 경우
+            emailVerificationCodes.remove(phoneNum);
+            codeExpirationTimes.remove(phoneNum);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(400, "expired", null));
+        }
+
+        String storedCode = emailVerificationCodes.get(phoneNum);
+        System.out.println(storedCode);
+        if (storedCode != null && storedCode.equals(code)) {
+            // 인증 성공 시 코드 제거
+            emailVerificationCodes.remove(phoneNum);
+            codeExpirationTimes.remove(phoneNum);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ApiResponse<>(200, "match", null));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse<>(400, "not_match", null));
+        }
+    }
+    
+    //아이디찾기 sms
+    @GetMapping("/checkPhone")
+    public ResponseEntity<ApiResponse<String>> checkPhone(@RequestParam("phone") String phone, @RequestParam("code") String code) {
+    	 String phoneNum = phone.replaceAll("[^0-9]", "");
         Long expirationTime = codeExpirationTimes.get(phoneNum);
         System.out.println(phoneNum+code);
         if (expirationTime != null && System.currentTimeMillis() > expirationTime) {
@@ -332,7 +361,7 @@ public class MemberController {
             System.out.println("Received data: " + dto.getName() + " " + dto.getPhone());
 
             String name = dto.getName();
-            String phone = dto.getPhone();
+            String phone = dto.getPhone().replaceAll("[^0-9]", "");
             
             Map<String, String> map = new HashMap<>();
             map.put("name", name);
