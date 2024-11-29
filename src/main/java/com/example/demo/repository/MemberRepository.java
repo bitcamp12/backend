8079a75cd65e4ea9748564300866252efd320b9d
luvoice97 <luvoice97@gmail.com>
2024-11-29 17:57:37 +0900

    엔티티

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -20,22 +22,21 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 
 	int countByNameAndEmailAndId(String name, String email, String id);
 
-    @Query("SELECT m.id FROM Member m WHERE m.name = :name AND m.email = :email")
-    String findIdByNameAndEmail(@Param("name") String name, @Param("email") String email);
+   
+    List<Member> findByNameAndEmail( String name,String email);
 
 	int countByNameAndPhone(String name, String phoneNum);
 
 	int countByNameAndPhoneAndId(String name, String phoneNum, String id);
 	
-	@Query("SELECT m.id FROM Member m WHERE m.name = :name AND m.phone = :phone")
-	String findIdByNameAndPhone(@Param("name")String name,  @Param("phone")String phone);
+
 	
-	@Modifying  // 업데이트, 삭제 등 데이터 변경 쿼리 실행 시 필수
-	@Transactional  // 데이터 변경이 트랜잭션 안에서 실행되도록 보장
-	@Query("UPDATE Member m SET m.password = :password WHERE m.id = :id")
-	int updatePasswordById(@Param("id") String id, @Param("password") String password);
 
 	int countByIdAndPassword(String id, String password);
+
+	Member findById(String id);
+
+	List<Member> findByNameAndPhone(String name, String phone);
 
 
 }
