9ef6cad1c99c0242f58d2bcecd4741a1e37118c8
imnotminda <dmc101110@gmail.com>
2024-12-09 14:26:56 +0900

    feature user (User main) 사용자 메인 탭 기능 백엔드

--- src/main/java/com/example/demo/dao/PlayDAO.java
+++ src/main/java/com/example/demo/dao/PlayDAO.java
@@ -42,4 +42,28 @@ public interface PlayDAO {
 			""")
     List<PlayDTO> getPlayRandom();
 
+    @Select("""
+			SELECT * FROM play 
+			WHERE end_time BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 30 DAY)
+			ORDER BY end_time ASC
+			LIMIT #{size} OFFSET #{offset}
+			""")
+	List<PlayDTO> getPlaysEndingSoon(@Param("offset") int offset, @Param("size") int size);
+
+	@Select("""
+			SELECT * FROM play
+			WHERE start_time > NOW()
+			ORDER BY start_time ASC
+			LIMIT #{size} OFFSET #{offset}
+			""")
+	List<PlayDTO> getPlaysComingSoon(@Param("offset") int offset, @Param("size") int size);
+
+	@Select("""
+			SELECT * FROM play
+			WHERE price >= 80000
+			ORDER BY price DESC
+			LIMIT #{size} OFFSET #{offset}
+			""")
+	List<PlayDTO> getPlaysLimited(int offset, int size);
+
 }
