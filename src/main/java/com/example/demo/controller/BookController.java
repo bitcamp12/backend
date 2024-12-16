0077b99d8afb5febc5bf960fbd16026beea9b440
imnotminda <dmc101110@gmail.com>
2024-12-16 17:21:15 +0900

    featuer user 결제 화면

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,18 +1,41 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.BookDTO;
+import com.example.demo.dto.TheaterDTO;
 import com.example.demo.service.BookService;
-
+import com.example.demo.util.ApiResponse;
 
 @RestController
-@RequestMapping(value="/api/books")
+@RequestMapping(value = "/api/books")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookController {
-    
+
 	@Autowired
 	private BookService bookService;
-	
 
+	@GetMapping(value = "getBookedSeats")
+	public ResponseEntity<ApiResponse<List<BookDTO>>> getBookedSeats(@RequestParam("playTimeTableSeq") int playTimeTableSeq) {
+		try {
+			List<BookDTO> list = bookService.getBookedSeats(playTimeTableSeq);
+
+			if (!list.isEmpty()) {
+				return ResponseEntity.ok(new ApiResponse<>(200, "성공", list));
+			} else {
+				return ResponseEntity.ok(new ApiResponse<>(404, "데이터 없음", list));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.ok(new ApiResponse<>(500, "서버 오류", null));
+		}
+	}
 }
--- src/main/java/com/example/demo/dao/BookDAO.java
+++ src/main/java/com/example/demo/dao/BookDAO.java
@@ -1,11 +1,34 @@
 package com.example.demo.dao;
 
-import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.dto.BookDTO;
 import com.example.demo.dto.member.MemberDTO;
 
 @Mapper
 public interface BookDAO {
+
+    @Select("""
+            SELECT 
+            ptt.theater_seq,
+            b.play_time_table_seq,
+            b.book_seq,
+            b.payment_status,
+            b.booked_x,
+            b.booked_y
+            FROM 
+                book b
+            INNER JOIN 
+                play_time_table ptt 
+                ON b.play_time_table_seq = ptt.play_time_table_seq
+            WHERE 
+                b.play_time_table_seq = #{playTimeTableSeq}
+                AND payment_status = 'PAID'
+            """)
+    List<BookDTO> getBookedSeats(int playTimeTableSeq);
 
 	
 }
