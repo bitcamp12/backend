6a2ce2774278939c054cf0cb3a2029e3f539a70e
imnotminda <dmc101110@gmail.com>
2024-12-18 18:48:46 +0900

    feature backend logging 구현 완료

--- src/main/java/com/example/demo/BackendApplication.java
+++ src/main/java/com/example/demo/BackendApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
