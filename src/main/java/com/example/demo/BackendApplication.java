16234749dd2ee2fcd4ef40dbcaf69677e9837f7a
unknown <tkp1200@naver.com>
2024-12-24 18:05:39 +0900

    diq

--- src/main/java/com/example/demo/BackendApplication.java
+++ src/main/java/com/example/demo/BackendApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @MapperScan({"com.example.demo.dao"})
 @SpringBootApplication
-@EnableCaching // Spring 캐싱을 활성화합니다.
 public class BackendApplication {
 
 	public static void main(String[] args) {
