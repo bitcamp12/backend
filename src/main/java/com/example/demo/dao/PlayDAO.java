09bee044e15bbce3d921adabfed2330b24a46eb5
unknown <tkp1200@naver.com>
2024-12-26 11:35:37 +0900

    diq

--- src/main/java/com/example/demo/dao/PlayDAO.java
+++ src/main/java/com/example/demo/dao/PlayDAO.java
@@ -18,10 +18,21 @@ public interface PlayDAO {
 
 	@Select("""
 
-			 SELECT   p.*, t.discounted_price,t.discount_rate,t.start_time,t.target_date
-    FROM   play p
-    LEFT JOIN play_time_table t  ON  p.play_seq = t.play_seq
-    WHERE  p.play_seq = #{playSeq}
+		SELECT 
+    p.*, 
+    t.discounted_price, 
+    t.discount_rate, 
+    t.start_time, 
+    t.target_date
+FROM 
+    play p
+LEFT JOIN 
+    play_time_table t ON p.play_seq = t.play_seq
+WHERE 
+    p.play_seq = #{playSeq}
+ORDER BY 
+    t.start_time ASC
+LIMIT 1;
 			""")
 	PlayDTO getPlayOne(String playSeq);
 
